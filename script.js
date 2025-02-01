e778214fb19784968a85290aabe4ef64d2f96ac3
alphacoderaman0 <amanmittal0004g@gmail.com>
2025-02-01 22:50:19 +0530

    More changes

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-const btn = document.querySelector('button');
-const input = document.querySelector('input');
+const btn = document.querySelector('#generate-btn');
+const input = document.querySelector('#url-input');
 const img = document.querySelector('#img');
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
+    if (input.value == "") {
+        alert("Please enter any text or URL");
+        img.innerHTML = `<img src="Screenshot 2024-03-19 145526.png" height="100px" width="100px">`;
+    } else {
+        fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(input.value)}`)
+            .then(response => {
+                return response.blob();
+            })
+            .then(blob => {
+                const objectUrl = URL.createObjectURL(blob);
+                img.innerHTML = `<img src="${objectUrl}" height="150px" width="150px">`;
+            });
 
-    if(input.value == ""){
-        alert("Please enter any text or Url"); 
-        img.innerHTML = `<img src="Screenshot 2024-03-19 145526.png" height="100px" width="100px">`; 
-    }
-    else{
-       fetch(` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="${input.value}"`)
-    .then(response=>{
-        return response.blob();
-    })
-    .then(blob=>{
-        const ObjectUrl = URL.createObjectURL(blob);
-        img.innerHTML = `<img src="${ObjectUrl}" height="100px" width="100px">`;
-        }); 
         alert("QR Generated");
-        }
-        
-        
-})
+    }
+});
